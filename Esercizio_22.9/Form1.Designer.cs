e6604704868b4effe3c8b22004f6d99626a9407d
matteogirotti <matteogirotti10@gmail.com>
2025-04-19 16:27:45 +0200

    Add project files.

---
+++ Esercizio_22.9/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Esercizio_22._9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcolaArea = new System.Windows.Forms.Button();
+            this.txtBase = new System.Windows.Forms.TextBox();
+            this.txtAltezza = new System.Windows.Forms.TextBox();
+            this.lblRisultato = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalcolaArea
+            // 
+            this.btnCalcolaArea.Location = new System.Drawing.Point(308, 95);
+            this.btnCalcolaArea.Name = "btnCalcolaArea";
+            this.btnCalcolaArea.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcolaArea.TabIndex = 0;
+            this.btnCalcolaArea.Text = "Calcola";
+            this.btnCalcolaArea.UseVisualStyleBackColor = true;
+            this.btnCalcolaArea.Click += new System.EventHandler(this.btnCalcolaArea_Click);
+            // 
+            // txtBase
+            // 
+            this.txtBase.Location = new System.Drawing.Point(85, 63);
+            this.txtBase.Name = "txtBase";
+            this.txtBase.Size = new System.Drawing.Size(100, 20);
+            this.txtBase.TabIndex = 1;
+            // 
+            // txtAltezza
+            // 
+            this.txtAltezza.Location = new System.Drawing.Point(85, 127);
+            this.txtAltezza.Name = "txtAltezza";
+            this.txtAltezza.Size = new System.Drawing.Size(100, 20);
+            this.txtAltezza.TabIndex = 2;
+            // 
+            // lblRisultato
+            // 
+            this.lblRisultato.AutoSize = true;
+            this.lblRisultato.Location = new System.Drawing.Point(457, 95);
+            this.lblRisultato.Name = "lblRisultato";
+            this.lblRisultato.Size = new System.Drawing.Size(0, 13);
+            this.lblRisultato.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblRisultato);
+            this.Controls.Add(this.txtAltezza);
+            this.Controls.Add(this.txtBase);
+            this.Controls.Add(this.btnCalcolaArea);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcolaArea;
+        private System.Windows.Forms.TextBox txtBase;
+        private System.Windows.Forms.TextBox txtAltezza;
+        private System.Windows.Forms.Label lblRisultato;
+    }
+}
+
---
+++ Esercizio_22.9/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Esercizio_22._9
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcolaArea_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtBase.Text, out double b) && double.TryParse(txtAltezza.Text, out double h))
+            {
+                double area = (b * h) / 2;
+                lblRisultato.Text = $"Area: {area}";
+            }
+            else
+            {
+                MessageBox.Show("Inserisci valori numerici validi.");
+            }
+        }
+    }
+}
